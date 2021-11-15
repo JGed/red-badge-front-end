81de53f182ef9ea96cced8de096aa8b05bb0d78d
Josh Geddes <josh.geddes96@gmail.com>
2021-11-15 17:50:44 -0500

    added fix to api url for deployed site

--- src/api/environment.ts
+++ src/api/environment.ts
@@ -5,7 +5,7 @@ switch(window.location.hostname) {
     case '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-    case 'jbg-vg-reviews-back.herokuapp.com/':
+    case 'jbg-vg-reviews.herokuapp.com/':
         APIURL = 'https://jbg-vg-reviews-back.herokuapp.com/';
         break;
     default: 
