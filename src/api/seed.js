46600ca8cbdf0f60eea2e5b25120c18f2128f362
Josh Geddes <josh.geddes96@gmail.com>
2021-11-16 16:12:02 -0500

    seed update

--- src/api/seed.js
+++ src/api/seed.js
@@ -11,55 +11,6 @@ const seed = async () => {
 
         // [{name: 'PS5', id: 167}, {name: 'PS4', id: 48}, {name: 'XONE', id: 49}, {name: 'XSX', id: 169}]) {
 
-        try {
-        const tresponse6 = await fetch(
-            'https://efa-cors-anywhere.herokuapp.com/https://api.igdb.com/v4/games',
-            {
-                method: 'POST',
-                headers: {
-                    Accept: 'application/json',
-                    'Client-ID': 'ggr8i4wd68qwqk6xahsj12kzt433y1',
-                    Authorization: 'Bearer mc2pq9jvuwd6isn6apyqxhvi1o43ve',
-                },
-                body: `fields name, cover, platforms, genres; where version_parent = null & platforms = ${169};`,
-            }
-        );
-        const tjson6 = await tresponse6.json();
-        for (const game of tjson6) {
-            const tresponse2 = await fetch(
-                'https://efa-cors-anywhere.herokuapp.com/https://api.igdb.com/v4/genres',
-                {
-                    method: 'POST',
-                    headers: {
-                        Accept: 'application/json',
-                        'Client-ID': 'ggr8i4wd68qwqk6xahsj12kzt433y1',
-                        Authorization: 'Bearer mc2pq9jvuwd6isn6apyqxhvi1o43ve',
-                    },
-                    body: `fields name; where id= ${game.genres?.[0] ?? 0};`,
-                }
-            );
-            const tjson2 = await tresponse2.json();
-            const genre = tjson2[0]?.name;
-            console.log(game, genre);
-            await fetch(`${APIURL}/game/`, {
-                method: 'POST',
-                headers: new Headers({
-                    'Content-Type': 'application/json',
-                    Authorization: adminAuth.token,
-                }),
-                body: JSON.stringify({
-                    game: {
-                        title: game.name,
-                        platform: 'XSX',
-                        genre: genre,
-                    },
-                }),
-            });
-        }
-    }
-    catch(e) {
-
-    }
     try {
 
         const tresponse7 = await fetch(
